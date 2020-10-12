b4bfb8048f92a415ef13e7488ddd2af8286cd107
Juergen <jre_developer@arcor.de>
2020-10-12 10:09:12 +0200

    adjusted offsets for my car

--- jetracer/nvidia_racecar.py
+++ jetracer/nvidia_racecar.py
@@ -3,25 +3,30 @@ import traitlets
 from adafruit_servokit import ServoKit
 
 
+
 class NvidiaRacecar(Racecar):
-    
-    i2c_address = traitlets.Integer(default_value=0x40)
-    steering_gain = traitlets.Float(default_value=-0.65)
-    steering_offset = traitlets.Float(default_value=0)
-    steering_channel = traitlets.Integer(default_value=0)
-    throttle_gain = traitlets.Float(default_value=0.8)
-    throttle_channel = traitlets.Integer(default_value=1)
-    
-    def __init__(self, *args, **kwargs):
-        super(NvidiaRacecar, self).__init__(*args, **kwargs)
-        self.kit = ServoKit(channels=16, address=self.i2c_address)
-        self.steering_motor = self.kit.continuous_servo[self.steering_channel]
-        self.throttle_motor = self.kit.continuous_servo[self.throttle_channel]
-    
-    @traitlets.observe('steering')
-    def _on_steering(self, change):
-        self.steering_motor.throttle = change['new'] * self.steering_gain + self.steering_offset
-    
-    @traitlets.observe('throttle')
-    def _on_throttle(self, change):
-        self.throttle_motor.throttle = change['new'] * self.throttle_gain
+   #set offsets for my car
+   #after changing values, re-run setup.py
+   i2c_address = traitlets.Integer(default_value=0x40)
+   steering_gain = traitlets.Float(default_value=1)
+   steering_offset = traitlets.Float(default_value=0.15)
+   steering_channel = traitlets.Integer(default_value=0)
+   throttle_gain = traitlets.Float(default_value=1)
+   throttle_channel = traitlets.Integer(default_value=1)
+   throttle_offset = traitlets.Float(default_value=0.13)
+
+   def __init__(self, *args, **kwargs):
+       super(NvidiaRacecar, self).__init__(*args, **kwargs)
+       self.kit = ServoKit(channels=16, address=self.i2c_address)
+       self.steering_motor = self.kit.continuous_servo[self.steering_channel]
+       self.throttle_motor = self.kit.continuous_servo[self.throttle_channel]
+       self.steering_motor.throttle = self.steering_offset
+       self.throttle_motor.throttle = self.throttle_offset
+
+   @traitlets.observe('steering')
+   def _on_steering(self, change):
+       self.steering_motor.throttle = change['new'] * self.steering_gain + self.steering_offset
+
+   @traitlets.observe('throttle')
+   def _on_throttle(self, change):
+       self.throttle_motor.throttle = change['new'] * self.throttle_gain + self.throttle_offset
